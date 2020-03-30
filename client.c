55a8f35bbf6dc99b9b7b4baa80132b50a0da4829
rippingcorn21 <pbegkas98@outlook.com>
2020-03-30 10:29:04 +0300

    completed client multi thread option

--- client.c
+++ client.c
@@ -88,24 +88,37 @@ void talk(const struct sockaddr_in server_addr, char *buffer) {
   close(socket_fd);
 }
 
+char *host = NULL;
+char *request = NULL;
+int mode = 0;
+int option = 0;
+// int count = ITER_COUNT;
+// char snd_buffer[BUF_SIZE];
+int station, value;
+struct sockaddr_in server_addr;
+struct hostent *host_info;
+int iteration;
 
 /////////////////////
 // request routine //
 /////////////////////
-int req(int iter,){
-
+void *req(){
+  int iter = iteration;
+  char snd_buffer[BUF_SIZE];
+  int sta;
+  int val;
   while(--iter>=0) {
-      for (station = 0; station <= MAX_STATION_ID; station++) {
+      for (sta = 0; sta <= MAX_STATION_ID; sta++) {
         memset(snd_buffer, 0, BUF_SIZE);
         //  half the requests are put and half  are get
-        if (station%2) {
+        if (sta%2) {
           // Repeatedly GET.
-          sprintf(snd_buffer, "GET:station.%d", station);
+          sprintf(snd_buffer, "GET:station.%d", sta);
         } else{
           // Repeatedly PUT.
           // create a random value.
-          value = rand() % 65 + (-20);
-          sprintf(snd_buffer, "PUT:station.%d:%d", station, value);
+          val = rand() % 65 + (-20);
+          sprintf(snd_buffer, "PUT:station.%d:%d", sta, val);
         }
         printf("Operation: %s\n", snd_buffer);
         talk(server_addr, snd_buffer);
@@ -120,20 +133,15 @@ int req(int iter,){
   return 0;
 }
 
+
 /**
  * @name main - The main routine.
  */
 int main(int argc, char **argv) {
-  char *host = NULL;
-  char *request = NULL;
-  int mode = 0;
-  int option = 0;
-  int count = ITER_COUNT;
-  char snd_buffer[BUF_SIZE];
-  int station, value;
-  struct sockaddr_in server_addr;
-  struct hostent *host_info;
   
+  char snd_buffer[BUF_SIZE];
+  int count = ITER_COUNT;
+
   // Parse user parameters.
   while ((option = getopt(argc, argv,"i:hgpor:a:")) != -1) {
     switch (option) {
@@ -209,11 +217,13 @@ int main(int argc, char **argv) {
     strncpy(snd_buffer, request, strlen(request));
     printf("Operation: %s\n", snd_buffer);
     talk(server_addr, snd_buffer);
-  } else if(mode == THREAD_MODE){ // here we do the thread mode operation, 1 iteration = 128 requests
+  } else if(mode == THREAD_MODE){ // here we do the thread mode operation
     //create multiple threads
     pthread_t tID;
-    for(int i; i < THREAD_COUNT; i ++){
-      pthread_create(&tID, NULL, req, count);
+    iteration = count;
+    // int *pCount = &count;
+    for(int i = 0; i < THREAD_COUNT; i ++){
+      pthread_create(&tID, NULL, req, NULL);
     }
     // wait
     pthread_mutex_lock(&mWait);
@@ -242,4 +252,7 @@ int main(int argc, char **argv) {
   }
   return 0;
 }
+
+
+
 
